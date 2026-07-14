5cdcc2eac32d6d250824a44f87e9fe99d0b816b2
Adi Maitre <adimaitre56@gmail.com>
2026-07-14 22:44:19 +0530

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/wasm';
 
 let prismaInstance: PrismaClient | undefined;
 
