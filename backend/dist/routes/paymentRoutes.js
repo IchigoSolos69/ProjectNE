d1f353b7261932a224e24e51bdfccaa4301039e6
Adi Maitre <adimaitre56@gmail.com>
2026-07-17 09:36:51 +0530

    reset

--- backend/dist/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const orderController_1 = require("../controllers/orderController");
-const router = (0, express_1.Router)();
-router.post("/create", orderController_1.createOrder);
-router.post("/verify", orderController_1.verifyPayment);
-exports.default = router;
--- backend/src/routes/paymentRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createOrder, verifyPayment } from "../controllers/orderController";
-
-const router = Router();
-
-router.post("/create", createOrder);
-router.post("/verify", verifyPayment);
-
-export default router;
