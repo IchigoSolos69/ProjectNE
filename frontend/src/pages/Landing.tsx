3d64542ef5cec4c4c105d70f9cce7a6a267732d0
Adi Maitre <adimaitre56@gmail.com>
2026-07-18 15:12:52 +0530

    Revert "blanket transition"
    This reverts commit 6385bbb2cc2f305f56f448b7a05157148bd21120.

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { HeroCarousel } from '../components/HeroCarousel';
+import { BlanketScrollSection } from '../components/BlanketScrollSection';
 import { TrendingGrid } from '../components/TrendingGrid';
 
 export const Landing: React.FC = () => {
   return (
     <main className="flex-1 bg-white">
-      {/* 1. Hero Carousel Split Layout with reveal overlay */}
+      {/* 1. Hero Carousel Split Layout */}
       <HeroCarousel />
 
-      {/* 2. Curator-Selected Trending Bedding Grid */}
+      {/* 2. Signature Blanket Scrub Unfurling Section */}
+      <BlanketScrollSection />
+
+      {/* 3. Curator-Selected Trending Bedding Grid */}
       <TrendingGrid />
     </main>
   );
